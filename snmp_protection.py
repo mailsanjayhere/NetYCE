506f623551cddc77db76d9503681af262f4a2d98
mailsanjayhere <33064356+mailsanjayhere@users.noreply.github.com>
2022-09-20 15:25:01 +0530

    Create snmp_protection.py

---
+++ snmp_protection.py
@@ -0,0 +1,24 @@
+import sys
+import re
+
+regex = '^[0-9]+$'
+not_digit=0
+def check(string):
+      # pass the regular expression
+     # and the string in search() method
+    if not (re.search(regex, string)):
+        not_digit = 1
+
+host = sys.argv[1]
+
+with open("/opt/yce/"+host,"r") as f:
+        config = f.readlines()
+        for line in config:
+                if "snmp-server community" in line:
+                        acl_no = line.split()[-1]
+                        check(acl_no)
+
+if not_digit == 1:
+        print('No')
+else:
+        print('Yes')
